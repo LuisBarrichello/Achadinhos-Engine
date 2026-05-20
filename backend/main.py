12f9934d69e7ad9c1f92ffc63faab5cf04099e17
Luis Barrichello <luisgbarrichelo@gmail.com>
2026-05-20 14:44:41 -0300

    refactor: refactor cors settings

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Achadinhos do Momento API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[FRONTEND_ORIGIN, "http://localhost:5500", "http://localhost:3000"],
     allow_methods=["GET", "POST", "PATCH", "DELETE"],
     allow_headers=["*"],
 )
